9b166a147ab13b8b89e9d483366661fc448db872
Heel Ghevariya <heelghevariya@gmail.com>
2026-04-15 16:03:55 +0200

    CTAS,VIEW,TEMP

---
+++ DDL_DML_test.sql
@@ -0,0 +1,23 @@
+CREATE TABLE staging.job_postings_flat as
+SELECT
+    jpf.job_id,
+    jpf.job_title_short,
+    jpf.job_title,
+    jpf.job_location,
+    jpf.job_via,
+    jpf.job_schedule_type,
+    jpf.job_work_from_home,
+    jpf.search_location,
+    jpf.job_posted_date,
+    jpf.job_no_degree_mention,
+    jpf.job_health_insurance,
+    jpf.job_country,
+    jpf.salary_rate,
+    jpf.salary_year_avg,
+    jpf.salary_hour_avg,
+    cd.name AS company_name
+FROM data_jobs.job_postings_fact AS jpf
+LEFT JOIN data_jobs.company_dim AS cd
+    ON jpf.company_id = cd.company_id; 
+
+SELECT COUNT(*) FROM staging.job_postings_flat;
